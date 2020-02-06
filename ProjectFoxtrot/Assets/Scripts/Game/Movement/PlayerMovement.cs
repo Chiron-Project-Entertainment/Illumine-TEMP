5b498198ddbf10ff32a91f316644b9e57982b6a7
Robert Toana (toanar) <toanar@coventry.ac.uk>
2020-02-06 17:45:19 +0000

    Update PlayerMovement.cs

--- ProjectFoxtrot/Assets/Scripts/Game/Movement/PlayerMovement.cs
+++ ProjectFoxtrot/Assets/Scripts/Game/Movement/PlayerMovement.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     #region Variables
-    [Header ("Components")]
+    [Header("Components")]
     [SerializeField] private Transform groundCheck = null;
     [SerializeField] private LayerMask groundMask = 512;
     private CharacterController controller = null;
     [SerializeField] private Animator headAnimator = null;
 
-    [Header ("General")]
+    [Header("General")]
     [SerializeField] private float gravity = 9.81f;
     [SerializeField] private float gravityWhileWallRunning = 4f;
     private float currentGravity = 0f;
@@ -20,38 +20,38 @@ public class PlayerMovement : MonoBehaviour
     private bool ceilingAbove = false;
     private float ignoreOnGroundTime = 0f;
 
-    [Header ("Multipliers")]
+    [Header("Multipliers")]
     [SerializeField] private float generalTransitionSpeed = 4f;
     [SerializeField] private float slidingTransitionSpeed = 0.75f;
-    [SerializeField][Range (1, 5)] private float sprintingMultiplier = 1.5f;
-    [SerializeField][Range (0, 1)] private float crouchingMultiplier = 0.5f;
+    [SerializeField] [Range(1, 5)] private float sprintingMultiplier = 1.5f;
+    [SerializeField] [Range(0, 1)] private float crouchingMultiplier = 0.5f;
     private readonly float baseMultiplier = 1f;
     private float currentMultiplier = 1f;
     private float targetMultiplier = 1f;
 
-    [Header ("Movement")]
-    [SerializeField][Range (0, 15)] private float forwardSpeed = 4.0f;
-    [SerializeField][Range (0, 15)] private float lateralSpeed = 2.0f;
+    [Header("Movement")]
+    [SerializeField] [Range(0, 15)] private float forwardSpeed = 4.0f;
+    [SerializeField] [Range(0, 15)] private float lateralSpeed = 2.0f;
     public bool IsSprinting { get; private set; }
-    public bool IsWalking { get { return !isSliding && !IsSprinting && OnGround && (Mathf.Abs (velocity.x) > 0.35f || Mathf.Abs (velocity.z) > 0.35f); } }
+    public bool IsWalking { get { return !isSliding && !IsSprinting && OnGround && (Mathf.Abs(velocity.x) > 0.35f || Mathf.Abs(velocity.z) > 0.35f); } }
     public bool IsWallRunning { get; private set; }
     /// <summary> Used for proper movement and calculations of wall sticking and running. </summary>
     private Vector3 wallRunningGlobalDirection = Vector3.zero;
     /// <summary> Used only for finding the correct wall running animations. </summary>
     private Vector3 wallRunningLocalDirection = Vector3.zero;
 
-    [Header ("Jumping")]
-    [SerializeField][Range (0, 6)] public float jumpHeight = 1.0f;
+    [Header("Jumping")]
+    [SerializeField] [Range(0, 6)] public float jumpHeight = 1.0f;
     public bool OnGround { get; private set; }
     private float timeInAir = 0f;
 
-    [Header ("Crouching & sliding")]
+    [Header("Crouching & sliding")]
     [SerializeField] private float standUpHeight = 1.8f;
     [SerializeField] private float crouchHeight = 1f;
     public bool IsCrouching { get; private set; }
     private bool isSliding = false;
-    [SerializeField][Range (0.05f, 1.0f)] private float slidingStartDifference = 0.30f;
-    [SerializeField][Range (0.05f, 1.0f)] private float slidingStopDifference = 0.15f;
+    [SerializeField] [Range(0.05f, 1.0f)] private float slidingStartDifference = 0.30f;
+    [SerializeField] [Range(0.05f, 1.0f)] private float slidingStopDifference = 0.15f;
     #endregion
 
 
@@ -59,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
     #region Methods
     private void Awake()
     {
-        controller = GetComponent<CharacterController> ();
+        controller = GetComponent<CharacterController>();
         controller.height = standUpHeight;
         currentGravity = gravity;
     }
@@ -69,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
     /// <summary> 
     /// Checks if the player can wall run in the given direction. Returns true to the wall if able to, else returns false.
     /// </summary>
-    private bool CanWallRun (Vector3 direction)
+    private bool CanWallRun(Vector3 direction)
     {
         return Physics.SphereCast(new Ray(transform.position, direction), controller.radius, controller.radius * 1.5f, groundMask, QueryTriggerInteraction.Ignore);
     }
@@ -80,8 +80,8 @@ public class PlayerMovement : MonoBehaviour
     {
         #region Updating status
         // Firstly, updating the current player status
-        OnGround = Physics.CheckSphere (groundCheck.position, groundDistance, groundMask, QueryTriggerInteraction.Ignore);
-        ceilingAbove = Physics.CheckSphere (transform.position + transform.up * standUpHeight, groundDistance, groundMask, QueryTriggerInteraction.Ignore);
+        OnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask, QueryTriggerInteraction.Ignore);
+        ceilingAbove = Physics.CheckSphere(transform.position + transform.up * standUpHeight, groundDistance, groundMask, QueryTriggerInteraction.Ignore);
         #endregion
 
         #region Resetting velocity, timeInAir and IsWallRunning.
@@ -95,7 +95,7 @@ public class PlayerMovement : MonoBehaviour
             // only to make sure that the player stays firmly onto the ground.
             if (velocity.y < 0) velocity.y = -2f;
 
-            if(timeInAir != 0f)
+            if (timeInAir != 0f)
             {
                 headAnimator.SetTrigger("Fell");
                 headAnimator.SetFloat("TimeInAir", timeInAir);
@@ -109,14 +109,14 @@ public class PlayerMovement : MonoBehaviour
         // Check if we wanna wall run or jump
         if (Controls.GetActionDown(UserAction.Jump) && OnGround && !IsWallRunning && !ceilingAbove)
         {
-            if(IsSprinting && !IsCrouching)
+            if (IsSprinting && !IsCrouching)
             {
                 // Check if there is any direction in which we can wall run.
-                foreach(var actionDir in new Dictionary<UserAction, Vector3>() { { UserAction.Left, -transform.right }, { UserAction.Right, transform.right }, { UserAction.Forward, transform.forward } })
+                foreach (var actionDir in new Dictionary<UserAction, Vector3>() { { UserAction.Left, -transform.right }, { UserAction.Right, transform.right }, { UserAction.Forward, transform.forward } })
                 {
-                    if(Controls.GetAction(actionDir.Key))
+                    if (Controls.GetAction(actionDir.Key))
                     {
-                        if(CanWallRun(actionDir.Value))
+                        if (CanWallRun(actionDir.Value))
                         {
                             wallRunningGlobalDirection = actionDir.Value;
                             if (actionDir.Value == -transform.right) wallRunningLocalDirection = Vector3.left;
@@ -129,7 +129,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             // If we can wall run, let's do it!
-            if(wallRunningGlobalDirection != Vector3.zero)
+            if (wallRunningGlobalDirection != Vector3.zero)
             {
                 velocity.y = Mathf.Sqrt(jumpHeight * 2 * currentGravity);
                 ignoreOnGroundTime = 2f;
@@ -144,7 +144,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Make sure we stick to the wall if we're wall running
-        if(IsWallRunning)
+        if (IsWallRunning)
         {
             controller.Move(wallRunningGlobalDirection * Time.deltaTime);
             currentGravity = gravityWhileWallRunning;
@@ -162,7 +162,7 @@ public class PlayerMovement : MonoBehaviour
         // Check if currently holding down sprint
         if (!IsCrouching)
         {
-            if (Controls.GetAction (UserAction.Sprint))
+            if (Controls.GetAction(UserAction.Sprint))
             {
                 IsSprinting = true;
             }
@@ -173,7 +173,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Check for crouching
-        if (Controls.GetActionDown (UserAction.Crouch))
+        if (Controls.GetActionDown(UserAction.Crouch))
         {
             controller.height = crouchHeight;
             groundCheck.position += transform.up * crouchHeight / 2;
@@ -186,7 +186,7 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
-        else if (IsCrouching && !ceilingAbove && !Controls.GetAction (UserAction.Crouch))
+        else if (IsCrouching && !ceilingAbove && !Controls.GetAction(UserAction.Crouch))
         {
             controller.height = standUpHeight;
             groundCheck.position -= transform.up * crouchHeight / 2;
@@ -196,7 +196,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Check if we should slide or not
-        if (IsCrouching && Mathf.Abs (currentMultiplier - sprintingMultiplier) <= slidingStartDifference)
+        if (IsCrouching && Mathf.Abs(currentMultiplier - sprintingMultiplier) <= slidingStartDifference)
         {
             isSliding = true;
             headAnimator.SetBool("IsSliding", true);
@@ -208,10 +208,10 @@ public class PlayerMovement : MonoBehaviour
             if (IsCrouching) targetMultiplier = crouchingMultiplier;
             else if (IsSprinting) targetMultiplier = sprintingMultiplier;
             else targetMultiplier = baseMultiplier;
-            currentMultiplier = Mathf.Lerp (currentMultiplier, targetMultiplier, (isSliding ? slidingTransitionSpeed : generalTransitionSpeed) * Time.deltaTime);
+            currentMultiplier = Mathf.Lerp(currentMultiplier, targetMultiplier, (isSliding ? slidingTransitionSpeed : generalTransitionSpeed) * Time.deltaTime);
 
             // If the speed got back to the crouching speed, stop sliding (if necessary)
-            if (Mathf.Abs (currentMultiplier - crouchingMultiplier) <= slidingStopDifference)
+            if (Mathf.Abs(currentMultiplier - crouchingMultiplier) <= slidingStopDifference)
             {
                 isSliding = false;
                 headAnimator.SetBool("IsSliding", false);
@@ -226,7 +226,7 @@ public class PlayerMovement : MonoBehaviour
 
         #region Update velocity and getting axis input
         // Gather input and make sure that the diagonal movement is functioning correctly.
-        Vector3 input = new Vector3 (Controls.GetAxis (InputAxis.Horizontal), 0, Controls.GetAxis (InputAxis.Vertical));
+        Vector3 input = new Vector3(Controls.GetAxis(InputAxis.Horizontal), 0, Controls.GetAxis(InputAxis.Vertical));
         if (input.magnitude > 1) input /= input.magnitude;
 
         // Modify the player velocity based on input.
@@ -265,14 +265,14 @@ public class PlayerMovement : MonoBehaviour
 
         #region Moving
         // Move the player based on the velocity
-        controller.Move ((velocity.x * transform.right + velocity.y * transform.up + velocity.z * transform.forward) * Time.deltaTime);
+        controller.Move((velocity.x * transform.right + velocity.y * transform.up + velocity.z * transform.forward) * Time.deltaTime);
         #endregion
 
         #region Updating animations
         // Update animations
-        headAnimator.SetBool ("IsSprinting", IsSprinting);
-        headAnimator.SetBool ("IsWalking", IsWalking);
-        headAnimator.SetBool ("OnGround", OnGround);
+        headAnimator.SetBool("IsSprinting", IsSprinting);
+        headAnimator.SetBool("IsWalking", IsWalking);
+        headAnimator.SetBool("OnGround", OnGround);
         headAnimator.SetBool("IsWallRunning", IsWallRunning);
         headAnimator.SetFloat("WallRunningDirX", wallRunningLocalDirection.x);
         headAnimator.SetFloat("WallRunningDirZ", wallRunningLocalDirection.z);
